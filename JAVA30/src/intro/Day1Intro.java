a58f23866992bb4766b840950cd8b86caf078dbc
Rinku <Rinku@LAPTOP-UMF0E84N>
2026-06-20 20:45:27 +0530

    Day 1: Hello world program

---
+++ JAVA30/src/intro/Day1Intro.java
@@ -0,0 +1,16 @@
+package intro;
+
+public class Day1Intro {
+
+	public static void main(String[] args) {
+		String name="Rinku G";
+		int age=19;
+		String course="BSC MSCs";
+		System.out.println("Hello world!");
+		System.out.println("My name is "+name);
+		System.out.println("I am "+age+" years old");
+		System.out.println("I am studying "+course);
+
+	}
+
+}
